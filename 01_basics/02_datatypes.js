407a4f60e321285ba47ba395c80d1a9221397b42
MVIKAS79 <vicky79vikas@gmail.com>
2025-08-03 18:26:49 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict";//treat all js code as newer version
+
+//alert(3+3)//it will give error,we are using nodejs ,not browser
+
+console.log(3
+    +
+     3)//code readbility should be high
+
+console.log("Vikas")
+
+let name="vikas"  //string
+let age=20 //int
+let isLoggedIn=false //bool
+let state;
+
+//number =>2 to power 53
+//if number is big use bigint
+//string =>""
+//boolen =>true/false
+//null =>standalone value
+//undefined=> 
+//symbol=> 
+console.log("TYPES OF DATA TYPES")
+console.log(typeof "vikas");
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
