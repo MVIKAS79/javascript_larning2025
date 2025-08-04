3cb4b36909415eaa119863befc36d0fbeaeacbbb
MVIKAS79 <vicky79vikas@gmail.com>
2025-08-04 16:49:54 +0000

    03_modified

--- 01_basics/03_converstionOperation.js
+++ 01_basics/03_converstionOperation.js
@@ -13,17 +13,56 @@
 // true=>1;false=>0
 //---------------------------------------------------------
 
-let isLoggedIn=1;
+//let isLoggedIn=1;
 
-let boolenIsLoggedIn=Boolean(isLoggedIn)
-console.log(boolenIsLoggedIn);
+//let boolenIsLoggedIn=Boolean(isLoggedIn)
+//console.log(boolenIsLoggedIn);
 
 //1=>true;0=>false;
 //""=>false
 //"vikas"=>true;
 //--------------------------------------------------------------
 
-let someNumber=33
+//let someNumber=33
 
-let stringNumber=String(someNumber);
-console.log(typeof stringNumber);
+//let stringNumber=String(someNumber);
+//console.log(stringNumber)
+///console.log(typeof stringNumber);
+
+
+//************************Operations ***************************
+
+let val=3
+let negVal=-val
+//console.log(negVal);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1="vikas"
+
+let str2=" hello"
+
+let str3=str1+str2;//concatination
+
+// console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2")
+// console.log("1"+"2")
+// console.log(1+2)
+
+// console.log(+true);
+// console.log(+"");
+
+
+let num1,num2,num3;
+
+num1=num2=num3=2+2;
+
+let gameCounter=100;
+++gameCounter;
+console.log(gameCounter);
