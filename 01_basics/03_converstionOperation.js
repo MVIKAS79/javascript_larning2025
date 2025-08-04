74748d313b347ff810a2a77bc736326c546dae76
MVIKAS79 <vicky79vikas@gmail.com>
2025-08-04 16:14:31 +0000

    converstion of data Types

---
+++ 01_basics/03_converstionOperation.js
@@ -0,0 +1,29 @@
+// let score=33;
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let marks="33erd"
+// console.log(typeof marks)
+// let valueInNumber=Number(marks);
+// console.log(typeof valueInNumber);
+
+// "33"=>33
+// "33abc"=>NaN
+// true=>1;false=>0
+//---------------------------------------------------------
+
+let isLoggedIn=1;
+
+let boolenIsLoggedIn=Boolean(isLoggedIn)
+console.log(boolenIsLoggedIn);
+
+//1=>true;0=>false;
+//""=>false
+//"vikas"=>true;
+//--------------------------------------------------------------
+
+let someNumber=33
+
+let stringNumber=String(someNumber);
+console.log(typeof stringNumber);
