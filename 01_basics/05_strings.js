4e930f1e35a3a7ebc3a13fe76e0f103a9fd7c988
MVIKAS79 <vicky79vikas@gmail.com>
2025-08-05 13:45:40 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,33 @@
+// const name ="vikas"
+// const repoCount=50
+
+// //console.log(name+repoCount+" value");
+// console.log(`hello my name is ${name} and my repocount is ${repoCount}`);
+
+
+const gameName=new String('Vikas-kannada-ballari');
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase);
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('t'));
+
+// const newString=gameName.substring(0,4);
+// console.log(newString);
+
+// const gameName1='hiteshhc'
+// const anotherString=gameName1.slice(-8,4);
+// console.log(anotherString);
+
+const newString1="   vikas   ";
+console.log(newString1);
+console.log(newString1.trim());
+
+const url="https://google.com/vikas%20google";
+
+console.log(url.replace('%20', '-'));
+
+console.log(gameName.split('-'));
