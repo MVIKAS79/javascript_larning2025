bc570cc3f371b7a821b6953d2b357861b9fbcbdc
MVIKAS79 <vicky79vikas@gmail.com>
2025-08-05 14:12:59 +0000

    nums and maths in js

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,37 @@
+
+//++++++++++++++++++ Numbers ++++++++++++++++++++++++
+// const score=400;
+// console.log(score);
+
+// const balance=new Number(1000);
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+// const otherNumber=123.8996;
+// console.log(otherNumber.toPrecision(3));
+
+// const hundreds=1000000;
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+//+++++++++++++++++++  MATHS   +++++++++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(3.2));
+// console.log(Math.ceil(3.2));
+// console.log(Math.floor(3.2));
+// console.log(Math.min(3,2,4,1));
+// console.log(Math.max(3,44,42));
+
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10)+1);
+
+const min=10;
+const max=20;
+console.log(Math.floor(Math.random()*(max-min+1)));
+//Math.random();
+
+
