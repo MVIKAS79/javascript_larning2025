a02fcccd928f4be8e808168b37c10ab8bc556fbd
MVIKAS79 <vicky79vikas@gmail.com>
2025-08-11 10:55:40 +0000

    scopes in js

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,12 @@
+let a=100
+const b=200
+var c =300
+if(true){
+    let a=10
+    const b=20
+    var c=30//it prints this not a 300 because overide so dont use var keyword in js
+}
+ 
+console.log(a);
+console.log(b);
+console.log(c);
