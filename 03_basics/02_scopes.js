5ddc53c9738d02e2cdbe892f3b8263e8105ab1f6
MVIKAS79 <vicky79vikas@gmail.com>
2025-08-14 09:36:34 +0000

    scopes in js

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,12 +1,48 @@
-let a=100
-const b=200
-var c =300
-if(true){
-    let a=10
-    const b=20
-    var c=30//it prints this not a 300 because overide so dont use var keyword in js
-}
+// let a=100
+// const b=200
+// var c =300
+// if(true){
+//     let a=10
+//     const b=20
+//     var c=30//it prints this not a 300 because overide so dont use var keyword in js
+// }
  
-console.log(a);
-console.log(b);
-console.log(c);
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+function one()
+{
+    const username="vikas";
+    function two()
+    {
+        const website= " youtube";
+        //console.log(username);
+    }
+    //console.log(website);
+    //two()
+}
+if(true)
+{
+    const username="vikas";
+    if(username==="vikas")
+    {
+        const website=" google";
+        //console.log(username+website);
+    }
+    //console.log(website);
+}
+//console.log(username);
+
+//++++++++++++++++++++++++++++++++ intresting ++++++++++++++++++++++++
+
+console.log(addone(5));
+function addone(num) {
+    return num+1;
+}
+
+
+addTwo(5);---->it will give a error
+const addTwo=function (num) {
+   return  num+2;
+}
