ee56c740480e6390c8a65b7d36a0b3c4258b03b3
MVIKAS79 <vicky79vikas@gmail.com>
2025-08-18 04:33:36 +0000

    control flows like if else etc

---
+++ 04_control_flow/one.js
@@ -0,0 +1,56 @@
+// if 
+
+// const isUserloggedIn=true
+// const temperature=41
+// if(isUserloggedIn)
+// {
+//     console.log("less than 50");  
+// }
+// console.log("temperature is greater than 50");
+//3!=2
+//<, >, <=, >=, ==, !=, ===
+
+
+// const score=200
+
+// if(score>100)
+// {
+//     const power="fly"
+//     console.log(`user Powe:${power}`);
+// }
+// console.log(`user Powe:${power}`);
+
+
+const balance=1000
+
+// if(balance>500)
+// {
+//     //console.log("test"),console.log("test2");
+// }
+
+// if(balance<500)
+// {
+//     console.log("less than");
+// }
+// else if(balance<750)
+// {
+//     console.log("less than 750");
+// }
+// else
+// {
+//     console.log("less than 1200");
+// }
+
+
+const UserloggedIn=true;
+const debitCard=true
+const loggedInfromGoogle=true;
+const loggedInfromEmail=true;
+if(UserloggedIn&&debitcard&&2==3)
+{
+    console.log("Allow to buy course");
+}
+if(loggedInfromGoogle||loggedInfromEmail)
+{
+    console.log("user logged in");
+}
