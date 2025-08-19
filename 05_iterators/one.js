a4df50e105dcf1d4af41326f91e94d9f8c949b5e
MVIKAS79 <vicky79vikas@gmail.com>
2025-08-19 06:38:04 +0000

    loops for

---
+++ 05_iterators/one.js
@@ -0,0 +1,75 @@
+// for
+for(let i=0;i<=10;i++)
+{
+    const element=i;
+    if(element==5)
+    {
+        //console.log("5 is best number");
+    }
+    //console.log(element);
+    
+}
+
+//console.log(element);//element not defined
+
+// for (let i = 0; i <=10; i++) {
+//     console.log(`outer loop value: ${i}`);
+//     for (let j = 0; j <=10; j++) {
+//        //console.log(`inner loop value${j} and inner loop ${i}`);
+//        console.log(i + "*" + j + "=" + i*j);
+//     }
+// }
+
+// let myArray=["vikas","ise","sit"];
+// console.log(myArray.length);
+// for (let index = 0; index < myArray.length; index++) {
+//     const element = myArray[index];
+//     console.log(element);  
+// }
+
+
+//break and continue
+
+// const month=3
+// switch (month) {
+//     case 1:
+//         console.log("january")
+//         break;
+//     case 2:
+//         console.log("febraury")
+//         break;
+//     case 3:
+//         console.log("march")
+//         break;
+//     case 4:
+//         console.log("april")
+//         break;
+//     case 5:
+//         console.log("june")
+//         break;
+
+//     default:
+//         console.log("default value");
+//         break;
+// }
+
+// for (let index = 0; index <=20; index++) {
+//     if(index==5)
+//     {
+//         console.log(`Detected 5`);
+//         break;
+//     }
+//     console.log(`value of i is ${index}`);
+// }
+
+
+// continue
+// for (let index = 0; index <=20; index++) {
+//     if(index==5)
+//     {
+//         console.log(`Detected 5`);
+//         continue;
+//     }
+//     console.log(`value of i is ${index}`);
+// }
+
