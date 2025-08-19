ea2738b65ae78821dd6d70a93511a7d5f9ca841b
MVIKAS79 <vicky79vikas@gmail.com>
2025-08-19 16:45:24 +0000

    maps in js

---
+++ 05_iterators/seven.js
@@ -0,0 +1,12 @@
+const myNumbers=[1,2,3,4,5,6,7,8,9,10]
+
+// const newNums=myNumbers.map( (num) => 
+// { 
+//     return num + 10
+// })
+const newNums=myNumbers
+              .map( (num) => num*10)
+              .map( (num) => num+1)
+              //.map( (num) => num>=40)//->>it print the true or false
+              .filter( (num) => num>=40)//-->>it prints numbers as output
+console.log(newNums)
