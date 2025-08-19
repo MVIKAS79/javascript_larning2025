68a7622b0c4494c0909918491480bd4f22da9d69
MVIKAS79 <vicky79vikas@gmail.com>
2025-08-19 06:38:22 +0000

    while loops

---
+++ 05_iterators/two.js
@@ -0,0 +1,13 @@
+//while loop
+
+// while (index<=10) {
+//     console.log(`value of index is ${index}`);
+//     index=index+2;
+// }
+
+// do while loop
+// let score=11;
+// do {
+//     console.log(`score is ${score}`);
+//     score++;
+// } while (score<=10);
